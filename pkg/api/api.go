cf3d88748e8b31d238863a52232f2eab8a0b0700
Narhzih <omosekejiolawale@gmail.com>
2021-10-25 04:32:16 +0100

    Added routes for pipes and bookmarks:

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -25,9 +25,24 @@ func (h *Handler) Register(routeGroup *gin.RouterGroup) {
 	routeGroup.GET("/test-route", TestCaller)
 	routeGroup.POST("/google/signup", h.SingUpWithGoogle)
 	routeGroup.POST("/google/singin", h.SignInWithGoogle)
+
 	authApi := routeGroup.Group("/auth")
 	authApi.Use(AuthRequired(h.service.JWTConfig.Key, h.logger))
 	authApi.GET("/auth-test", TestCaller)
+
+	pipe := routeGroup.Group("/pipe")
+	pipe.Use(AuthRequired(h.service.JWTConfig.Key, h.logger))
+	pipe.POST("/", h.CreatePipe)
+	pipe.GET("/:id", h.GetPipe)
+	pipe.PUT("/:id", h.UpdatePipe)
+	pipe.GET("/all", h.GetPipes)
+
+	pipe.GET("/:id/bookmarks", h.GetBookmarks)
+	pipe.POST("/:id/bookmark", h.CreateBookmark)
+	pipe.GET("/:id/bookmark/:bmId", h.GetBookmark)
+	pipe.PUT("/:id/bookmark/:bmId", h.UpdatePipe)
+	pipe.DELETE("/:id/bookmark/:bmId", h.DeleteBookmark)
+
 }
 
 func TestCaller(c *gin.Context) {
---
+++ pkg/api/bookmark.go
@@ -0,0 +1,8 @@
+package api
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) CreateBookmark(c *gin.Context) {}
+func (h *Handler) GetBookmark(c *gin.Context)    {}
+func (h *Handler) GetBookmarks(c *gin.Context)   {}
+func (h *Handler) DeleteBookmark(c *gin.Context) {}
