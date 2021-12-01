5405687aa1129bd613b25b16c0fed394b8fafc7f
Narhzih <omosekejiolawale@gmail.com>
2021-12-01 17:54:51 +0100

    Temporarily removing manual env load from main.go

--- main.go
+++ main.go
@@ -12,7 +12,8 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
-	"github.com/joho/godotenv"
+
+	// "github.com/joho/godotenv"
 	"github.com/rs/zerolog"
 	"gitlab.com/gowagr/mypipe-api/db"
 	"gitlab.com/gowagr/mypipe-api/pkg/api"
@@ -20,7 +21,7 @@ import (
 )
 
 func main() {
-	godotenv.Load(".env")
+	// godotenv.Load(".env")
 	logger := zerolog.New(os.Stderr).With().Caller().Timestamp().Logger()
 	db, err := initDb(logger)
 	if err != nil {
