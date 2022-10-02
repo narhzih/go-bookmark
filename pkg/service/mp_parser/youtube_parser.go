130c2ac38e0bf3bcd404dd883ebc360124bb0142
narhzih <omosekejiolawale@gmail.com>
2022-10-02 13:05:53 +0100

    Refacotred youtube api to return video duration

--- pkg/service/mp_parser/youtube_parser.go
+++ pkg/service/mp_parser/youtube_parser.go
@@ -3,14 +3,18 @@ package mp_parser
 import (
 	"fmt"
 	"github.com/rs/zerolog"
+	_ "google.golang.org/api/youtube/v3"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"regexp"
 )
 
 func ParseYoutubeLink(youtubeLink string) (string, error) {
-	resp, err := http.Get(fmt.Sprintf("https://www.youtube.com/oembed?url=%+v&format=json", youtubeLink))
+	pattern, _ := regexp.Compile("(?:https?:\\/{2})?(?:w{3}\\.)?youtu(?:be)?\\.(?:com|be)(?:\\/watch\\?v=|\\/)([^\\s&]+)")
+	videoId := pattern.FindAllStringSubmatch(youtubeLink, -1)[0][1]
+	resp, err := http.Get(fmt.Sprintf("https://www.googleapis.com/youtube/v3/videos?id=%v&key=%v&part=snippet,contentDetails,statistics,status", videoId, os.Getenv("YOUTUBE_API_KEY")))
 	if err != nil {
 		return "", err
 	}
@@ -20,6 +24,7 @@ func ParseYoutubeLink(youtubeLink string) (string, error) {
 
 		}
 	}(resp.Body)
+
 	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		logger := zerolog.New(os.Stderr).With().Caller().Timestamp().Logger()
