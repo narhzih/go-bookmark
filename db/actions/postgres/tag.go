cdeddba1ef7509d803317f2208f4b7a2ca2ffb04
narhzih <omosekejiolawale@gmail.com>
2022-10-25 09:49:10 +0100

    Added tag query actions

--- db/actions/postgres/tag.go
+++ db/actions/postgres/tag.go
@@ -18,11 +18,44 @@ func NewTagActions(db *sql.DB, logger zerolog.Logger) repository.TagRepository {
 
 func (t tagActions) CreateTag(tag models.Tag) (models.Tag, error) {
 	var createdTag models.Tag
+	query := `
+	INSERT INTO tags (name)
+	VALUES ($1)
+	RETURNING id, name, created_at, modified_at
+	`
 
+	err := t.Db.QueryRow(query, tag.Name).Scan(
+		&tag.Id,
+		&tag.Name,
+		&tag.CreatedAt,
+		&tag.ModifiedAt,
+	)
+	if err != nil {
+		return tag, err
+	}
 	return createdTag, nil
 }
+
 func (t tagActions) GetTag(tagId string) (models.Tag, error) {
 	var tag models.Tag
+	query := `
+	SELECT id, name, created_at, modified_at 
+	FROM tags
+	WHERE id=$1
+    `
+
+	err := t.Db.QueryRow(query, tagId).Scan(
+		&tag.Id,
+		&tag.Name,
+		&tag.CreatedAt,
+		&tag.ModifiedAt,
+	)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return tag, ErrNoRecord
+		}
+		return tag, err
+	}
 
 	return tag, nil
 }
