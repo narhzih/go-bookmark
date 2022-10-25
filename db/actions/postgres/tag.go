5b59d4171e4fa622e6d457e494120be1233fd677
narhzih <omosekejiolawale@gmail.com>
2022-10-25 10:19:44 +0100

    Added tag query to add tags to bookmarks

--- db/actions/postgres/tag.go
+++ db/actions/postgres/tag.go
@@ -36,7 +36,7 @@ func (t tagActions) CreateTag(tag models.Tag) (models.Tag, error) {
 	return createdTag, nil
 }
 
-func (t tagActions) GetTag(tagId string) (models.Tag, error) {
+func (t tagActions) GetTag(tagId int64) (models.Tag, error) {
 	var tag models.Tag
 	query := `
 	SELECT id, name, created_at, modified_at 
@@ -58,4 +58,60 @@ func (t tagActions) GetTag(tagId string) (models.Tag, error) {
 	}
 
 	return tag, nil
+}
+
+func (t tagActions) GetTagByName(name string) (models.Tag, error) {
+	var tag models.Tag
+	query := `
+	SELECT id, name, created_at, modified_at 
+	FROM tags
+	WHERE name=$1
+	LIMIT 1
+    `
+
+	err := t.Db.QueryRow(query, name).Scan(
+		&tag.Id,
+		&tag.Name,
+		&tag.CreatedAt,
+		&tag.ModifiedAt,
+	)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return tag, ErrNoRecord
+		}
+		return tag, err
+	}
+
+	return tag, nil
+}
+
+func (t tagActions) AddTagsToBookmark(bmId string, tags []models.Tag) error {
+	// This call assumes the bookmark exists already
+	for _, tag := range tags {
+
+		tag, err := t.GetTagByName(tag.Name)
+		if err != nil {
+			// if there's not existing tag, create one
+			if err == ErrNoRecord {
+				tag, err = t.CreateTag(tag)
+				if err != nil {
+					return err
+				}
+			}
+
+			return err
+		}
+
+		query := `
+		INSERT INTO bookmark_tag (bookmark_id, tag_id)
+		VALUES ($1, $2)
+		RETURNING id, bookmark_id, tag_id, created_at, modified_at
+		`
+
+		_, err = t.Db.Exec(query, bmId, tag.Id)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
--- db/repository/tag.go
+++ db/repository/tag.go
@@ -4,5 +4,7 @@ import "github.com/mypipeapp/mypipeapi/db/models"
 
 type TagRepository interface {
 	CreateTag(tag models.Tag) (models.Tag, error)
-	GetTag(tagId string) (models.Tag, error)
+	GetTag(tagId int64) (models.Tag, error)
+	GetTagByName(name string) (models.Tag, error)
+	AddTagsToBookmark(bmId string, tags []models.Tag) error
 }
