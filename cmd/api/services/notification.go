e9dc9984cfffbd94764ee5a56f16acd0c81941b7
narhzih <omosekejiolawale@gmail.com>
2022-11-12 09:19:25 +0100

    Changed profile_name to username on pipe_share notification

--- cmd/api/services/notification.go
+++ cmd/api/services/notification.go
@@ -41,7 +41,7 @@ func (s Services) CreatePrivatePipeShareNotification(sharedPipeId, sharerId, sha
 		Code:   pipeShareRecord.Code,
 	}
 	mdToJson, _ := json.Marshal(metadata)
-	message := sharer.ProfileName + " privately shared you pipe with name: " + sharedPipe.Name
+	message := sharer.Username + " privately shared you pipe with name: " + sharedPipe.Name
 	_, err = s.Repositories.Notification.CreateNotification(sharedToId, message, string(mdToJson))
 	if err != nil {
 		return err
